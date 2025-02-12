054358ddf74300765448623460f1e52917e04827
Dale Crockwell <eladcrock@gmail.com>
2025-02-12 00:16:54 -0800

    package.json and config file update

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/joeys/', // This should match your GitHub repo name
+
   plugins: [react()],
   build: {
     // Add any specific build configuration options if necessary
